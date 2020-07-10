4136bd79d922bf8ca4ffc20ec23d76863312db9c
Dalia Sawaya <dalia.sawaya@flatironschool.com>
2020-07-10 12:44:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
+require 'pry'
 
-def countdown
+def countdown(number_of_seconds)
+  while number_of_seconds > 0
+    puts "#{number_of_seconds} SECOND(S)!"
+    number_of_seconds -= 1
+  end
+  
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number_of_seconds)
+  while number_of_seconds > 0
+    puts "#{number_of_seconds} SECOND(S)!"
+    number_of_seconds -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
